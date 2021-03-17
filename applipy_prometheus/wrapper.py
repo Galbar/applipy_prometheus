e40c0fa691d53f962b380140564dc20be179fa13
Alessio Linares <gitlab@alessio.cc>
2021-03-18 00:05:21 +0100

    use Chronometer as context manager

--- applipy_prometheus/wrapper.py
+++ applipy_prometheus/wrapper.py
@@ -5,6 +5,7 @@ from aiohttp import web
 from applipy import Config
 from applipy_http import Context, EndpointMethod, EndpointWrapper
 from applipy_metrics import Chronometer, MetricsRegistry
+from applipy_metrics.meters import BaseMetric
 
 
 class MetricsWrapper(EndpointWrapper):
@@ -22,29 +23,27 @@ class MetricsWrapper(EndpointWrapper):
 
         @functools.wraps(handler)
         async def wrapper(request: web.Request, context: Context) -> web.StreamResponse:
-            chrono = Chronometer()
             status = 500
-            try:
-                _tags = tags.copy()
-                _tags['server'] = context.get('server.name') or ''
-                context['metrics.tags'] = _tags
-                response = await handler(request, context)
-                status = response.status
-            except web.HTTPException as e:
-                status = e.status_code
-                raise
-            except Exception:
-                status = 500
-                raise
-            finally:
-                elapsed = chrono.stop()
-                _tags['status'] = status
-
-                self._metrics.summary('applipy_web_request_duration_seconds', _tags).add(elapsed)
-
+            _tags = tags.copy()
+            _tags['server'] = context.get('server.name') or ''
+            context['metrics.tags'] = _tags
+            with Chronometer(on_stop=lambda v: self._observe_value(_tags, v)):
+                try:
+                    response = await handler(request, context)
+                    status = response.status
+                except web.HTTPException as e:
+                    status = e.status_code
+                    raise
+                except Exception:
+                    status = 500
+                    raise
+                finally:
+                    _tags['status'] = status
             return response
-
         return wrapper
+
+    def _observe_value(self, tags, value) -> BaseMetric:
+        return self._metrics.summary('applipy_web_request_duration_seconds', tags).add(value)
 
     def priority(self) -> int:
         return 100
